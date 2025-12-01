4d8038e82ee99bd843f29dfac278f05193c2557c
thesujalpatel <patelsujal24102004@gmail.com>
2025-12-01 15:53:00 +0530

    Add book post type and genre taxonomy with custom fields support; enqueue styles and scripts

--- wp-content/plugins/bookstore/bookstore.php
+++ wp-content/plugins/bookstore/bookstore.php
@@ -27,8 +27,67 @@ function bookstore_register_book_post_type()
         "public" => true,
         "has_archive" => true,
         "show_in_rest" => true,
-        "supports" => ["title", "editor", "author", "thumbnail", "excerpt"],
+        "supports" => [
+            "title",
+            "editor",
+            "author",
+            "thumbnail",
+            "excerpt",
+            "custom-fields",
+        ],
     ];
 
     register_post_type("book", $args);
+}
+
+add_action("init", "bookstore_register_genre_taxonomy");
+function bookstore_register_genre_taxonomy()
+{
+    $args = [
+        "labels" => [
+            "name" => "Genres",
+            "singular_name" => "Genre",
+            "edit_item" => "Edit Genre",
+            "update_item" => "Update Genre",
+            "add_new_item" => "Add New Genre",
+            "new_item_name" => "New Genre Name",
+            "menu_name" => "Genres",
+            "all_items" => "All Genres",
+            "view_item" => "View Genre",
+        ],
+        "hierarchical" => true,
+        "rewrite" => ["slug" => "genre"],
+        "show_in_rest" => true,
+    ];
+
+    register_taxonomy("genre", "book", $args);
+}
+
+add_filter("postmeta_form_keys", "bookstore_add_isbn_to_quick_edit", 10, 2);
+function bookstore_add_isbn_to_quick_edit($keys, $post)
+{
+    if ($post->post_type === "book") {
+        $keys[] = "isbn";
+    }
+    return $keys;
+}
+
+// Styles
+add_action("wp_enqueue_scripts", "bookstore_enqueue_styles");
+function bookstore_enqueue_styles()
+{
+    wp_enqueue_style(
+        "bookstore-styles",
+        plugin_dir_url(__FILE__) . "bookstore-styles.css",
+    );
+}
+
+// Scripts
+add_action("wp_enqueue_scripts", "bookstore_enqueue_scripts");
+function bookstore_enqueue_scripts()
+{
+    wp_enqueue_script(
+        "bookstore-scripts",
+        plugin_dir_url(__FILE__) . "bookstore-scripts.js",
+    );
 }
